c86c8116c15b9d9823048ecca79cd8bdd7b4c979
Bilel <bileldounar@gmail.com>
2026-06-11 14:17:09 +0200

    style: supprimer max-width du header pour layout pleine largeur

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import HeaderAuthActions from "@/components/HeaderAuthActions";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 glass border-b border-[#252525]/80">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 xl:px-12">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8 xl:px-12">
         <div className="flex items-center justify-between h-14 lg:h-16">
           {/* Logo avec glow effect au hover */}
           <Link
